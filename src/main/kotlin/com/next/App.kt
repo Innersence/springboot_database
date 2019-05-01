f2c0ab76e5f3631cc2afdac6de92122b84059f10
Innersence <liyiping1988@126.com>
2019-05-01 20:12:33 +0800

    add kotlin starter

--- src/main/kotlin/com/next/App.kt
+++ src/main/kotlin/com/next/App.kt
@@ -1,2 +1,12 @@
 package com.next
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.SpringApplication
+
+
+@SpringBootApplication
+class KotlinServerApplication
+
+fun main(args: Array<String>){
+    SpringApplication.run(KotlinServerApplication::class.java, *args)
+}
